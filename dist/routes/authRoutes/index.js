04599bf0f2bc3b39d1f9ca1d68ae9c0cc165c8e0
yeremiadio <raikkonendio@gmail.com>
2024-08-15 21:09:00 +0700

    fix: remove dist

--- dist/routes/authRoutes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("@/controllers/authController");
-const authRoutes = (0, express_1.Router)();
-authRoutes.post('/register', authController_1.register);
-authRoutes.post('/login', authController_1.login);
-exports.default = authRoutes;
-//# sourceMappingURL=index.js.map
